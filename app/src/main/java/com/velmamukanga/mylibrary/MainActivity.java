06d3a498d46fc7ef0ae76fc7a4203ad4573353eb
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-02 01:24:07 +0300

    add butterknife for binding to signup page

--- app/src/main/java/com/velmamukanga/mylibrary/MainActivity.java
+++ app/src/main/java/com/velmamukanga/mylibrary/MainActivity.java
@@ -2,13 +2,34 @@ package com.velmamukanga.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/velmamukanga/mylibrary/SignUp.java
@@ -0,0 +1,35 @@
+package com.velmamukanga.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUp extends AppCompatActivity {
+
+
+    @BindView(R.id.button)
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        ButterKnife.bind(this);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
