67271ccbcdb1661a3ddb9dbd499a79b94b61edc2
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-02 14:55:37 +0300

    add all new changes done

--- app/src/main/java/com/velmamukanga/mylibrary/BooksActivity.java
+++ app/src/main/java/com/velmamukanga/mylibrary/BooksActivity.java
@@ -11,8 +11,8 @@ public class BooksActivity extends AppCompatActivity {
     ListView velma;
     BooksArrayAdapter akoch;
 
-    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole"};
-    private String[]cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
+    private String[] restaurant = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole"};
+    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican" };
 
 
 
@@ -21,8 +21,9 @@ public class BooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
         velma = findViewById(R.id.ListView);
-        akoch = new BooksArrayAdapter(BooksActivity.this, android.R.layout.simple_list_item_1,restaurants,cuisines);
+        akoch = new BooksArrayAdapter(BooksActivity.this, android.R.layout.simple_list_item_1, restaurant, cuisines);
         velma.setAdapter(akoch);
 
     }
 }
+
--- app/src/main/java/com/velmamukanga/mylibrary/LoginActivity.java
+++ app/src/main/java/com/velmamukanga/mylibrary/LoginActivity.java
@@ -14,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @BindView(R.id.button3)
     Button button2;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
