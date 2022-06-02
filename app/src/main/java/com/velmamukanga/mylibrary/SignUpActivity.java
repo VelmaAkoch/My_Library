3efed1462fd81667d4c90836629e2ec2699f822e
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-02 13:36:54 +0300

    renamed SigUpActivity

--- app/src/main/java/com/velmamukanga/mylibrary/SignUpActivity.java
+++ app/src/main/java/com/velmamukanga/mylibrary/SignUpActivity.java
@@ -6,28 +6,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.velmamukanga.mylibrary.validate.Validator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignUp extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
 
     @BindView(R.id.button2)
     Button button2;
+
+    @BindView(R.id.velma)
+    TextInputLayout name;
+
+    @BindView(R.id.editTextTextEmailAddress)
+   EditText email;
+
+    @BindView(R.id.editTextTextPassword)
+    EditText password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        Validator susan = new Validator();
 
         ButterKnife.bind(this);
         Button button = (Button)findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener(){
+
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUp.this, LoginActivity.class);
+
+                if (!susan.validateName(name) || ! susan.validateEmail(email) || ! susan.validatePassword(password)){
+                    return;
+                }
+                String username = name.getEditText().getText().toString();
+                Toast.makeText(SignUpActivity.this,username,Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
