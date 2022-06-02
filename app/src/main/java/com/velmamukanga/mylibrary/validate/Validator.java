fa786f4c9d43ff31c81c490add7de7c1d0dd8691
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-02 13:45:21 +0300

    add validations to sign up inputs

--- app/src/main/java/com/velmamukanga/mylibrary/validate/Validator.java
+++ app/src/main/java/com/velmamukanga/mylibrary/validate/Validator.java
@@ -1,2 +1,43 @@
-package com.velmamukanga.mylibrary.validate;public class Validator {
+package com.velmamukanga.mylibrary.validate;
+
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.jar.Attributes;
+
+public class Validator {
+    public boolean validateName(TextInputLayout name){
+        if (name.getEditText().getText().toString().isEmpty()){
+            name.setError("please enter your name");
+            return false;
+        }
+        return true;
+
+
+    }
+
+    public boolean validateEmail(EditText name){
+        if (name.getText().toString().isEmpty()){
+            name.setError("please enter your email");
+            return false;
+        }
+        return true;
+
+
+    }
+
+    public boolean validatePassword(EditText name){
+        if (name.getText().toString().isEmpty()){
+            name.setError("please enter your password");
+            return false;
+        }
+        else if (name.getText().toString().length()<8){
+            name.setError("password too small");
+            return false;
+        }
+        return true;
+
+
+    }
 }
