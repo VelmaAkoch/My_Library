f57f9c0bf013ccd41d1f96c276c51c94a91610b0
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-02 13:44:25 +0300

    create ana adapter to bind data tolist view

--- app/src/main/java/com/velmamukanga/mylibrary/adapters/BooksArrayAdapter.java
+++ app/src/main/java/com/velmamukanga/mylibrary/adapters/BooksArrayAdapter.java
@@ -1,2 +1,37 @@
-package com.velmamukanga.mylibrary.adapters;public class BooksArrayAdapter {
+package com.velmamukanga.mylibrary.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+
+import androidx.annotation.NonNull;
+
+import java.sql.Array;
+
+public class BooksArrayAdapter extends ArrayAdapter {
+    String[] books;
+    String[] booksDescription;
+    Context context;
+
+
+    public BooksArrayAdapter(@NonNull Context context, int resource, String[] books, String[] booksDescription) {
+        super(context, resource);
+        this.books = books;
+        this.booksDescription = booksDescription;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String book = books [position];
+        String bookDescription = booksDescription[position];
+        return String.format("%s \nServes great: %s", book , bookDescription);
+    }
+
+    @Override
+    public int getCount() {
+        return books .length;
+    }
 }
